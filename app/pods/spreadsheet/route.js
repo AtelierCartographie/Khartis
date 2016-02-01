88da7bf3f9ec7c1d9b4f0d879795af8392199c25
Arnaud PEZEL <arnaud@apyx.fr>
2016-02-01 16:21:27 +0100

    Nouvelle mise en page

--- app/pods/spreadsheet/route.js
+++ app/pods/spreadsheet/route.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
     renderTemplate: function() {
         this.render("spreadsheet/sidebar", { outlet: "sidebar" });
         this.render({ outlet: "main" });
-        this.render("spreadsheet/help", { outlet: "help" });
+        //this.render("spreadsheet/help", { outlet: "help" });
     },
     
     actions: {
