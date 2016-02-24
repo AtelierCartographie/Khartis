1f8f34d48008e60abcf6ce4a2db2343e9e96a272
Arnaud PEZEL <arnaud@apyx.fr>
2016-02-24 10:04:36 +0100

    amcorrection erreur bloquante compilation

--- app/utils/d3lper.js
+++ app/utils/d3lper.js
@@ -1,22 +1,18 @@
-export default = {
+export default {
 	
-	scale: function(val) {
-		
+	scale(val) {
 		return "scale("+val+")";
-		
 	},
 	
-	translate: function(tx, ty) {
-		
+	translate(tx, ty) {
 		return "translate("+tx+", "+ty+")";
-		
 	},
 	
-	yiqColor: function (d3Color) {
-	  var r = d3Color.r,
+	yiqColor(d3Color) {
+	  let r = d3Color.r,
 	      g = d3Color.g,
-	      b = d3Color.b;
-	  var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+	      b = d3Color.b,
+	      yiq = (r * 299 + g * 587 + b * 114) / 1000;
 	  return (yiq >= 128) ? 'darken' : 'lighten';
 	}
 	
