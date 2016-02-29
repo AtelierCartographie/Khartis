46739cf98466006127c985bd43fe63504ec690d5
Raphael Pora <raphael.pora@gmail.com>
2016-02-29 13:51:17 +0100

    tooltips / dropdowns / styles

--- app/pods/components/x-dropdown/x-trigger/component.js
+++ app/pods/components/x-dropdown/x-trigger/component.js
@@ -1,11 +1,10 @@
+/* global $ */
+
 // TODO: DropDown Ember position param
-// TODO: Close dropdown on action click
-// TODO: Close on window click when opened
 
 import Ember from 'ember';
 import snap, {forceUpdate} from 'mapp/utils/snap';
 
-/* global $ */
 
 export default Ember.Component.extend({
 
@@ -14,6 +13,8 @@ export default Ember.Component.extend({
   targetEl: null,
   snapped:null,
   open: false,
+  dropdownPivot:'top right',
+  triggerPivot:'bottom right',
 
   didInsertElement: function () {
 
@@ -22,33 +23,73 @@ export default Ember.Component.extend({
     this.set('triggerEl', trigger)
     this.set('targetEl', dropdown)
 
-    const toggle = this.toggle.bind(this)
+    this.toggle = this.toggle.bind(this)
+    this.handleOuterClick = this.handleOuterClick.bind(this)
 
-    snap(dropdown[0], 'top right').to(trigger[0], 'bottom right')
-    trigger.on('click', toggle)
+    snap(dropdown[0], this.get('dropdownPivot')).to(trigger[0],  this.get('triggerPivot'))
 
+    trigger.on('click', this.toggle)
+    dropdown.on('click', this.toggle)
+  },
+
+
+  handleOuterClick(e){
+
+      var dropdown = this.get('targetEl')
+      var trigger = this.get('triggerEl')
+      var target = e.target
+
+      do {
+        if($(target).is(dropdown) || $(target).is(trigger)) {
+          break
+        }
+      } while(target = target.parentNode)
+
+      // Target IS the dropdown or the trigger
+      if(target){
+        return
+      }
+
+      // Target can be everything else
+      this.hide()
   },
 
   toggle(){
     var isOpen = this.get('open')
     isOpen ? this.hide() : this.show()
-    this.toggleProperty('open')
-
   },
 
   show(){
+
+    $(document).on('click', this.handleOuterClick)
+
+    this.set('open', true)
     this.get('targetEl').addClass('visible')
     this.get('triggerEl').addClass('active')
   },
 
   hide(){
+
+    $(document).off('click', this.handleOuterClick)
+
+    this.set('open', false)
     this.get('targetEl').removeClass('visible')
     this.get('triggerEl').removeClass('active')
-
   },
 
   '$': function () {
     return $("#" + this.elementId);
+  },
+
+  willDestroyElement(){
+
+    var dropdown = this.get('targetEl')
+    var trigger = this.get('triggerEl')
+
+    // Make sure document click will not leak
+    $(document).off('click', this.handleOuterClick)
+    trigger.off('click', this.toggle)
+    dropdown.off('click', this.toggle)
   }
 
 });
