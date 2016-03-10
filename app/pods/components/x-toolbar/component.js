5fe5bfeebb8ddd3cd721e5115dbc5bc2a22bd864
Raphael Pora <raphael.pora@gmail.com>
2016-03-10 11:49:38 +0100

    FIX #44  Panel d'outils pour carte

---
+++ app/pods/components/x-toolbar/component.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+/* global $ */
+
+export default Ember.Component.extend({
+  tagName: "div",
+  classNames:['toolbar'],
+  triggerEl:null,
+
+  didInsertElement(){
+    var trigger = this.$('.toolbar-trigger')
+    this.set('triggerEl', trigger)
+
+    this.toggle = this.toggle.bind(this)
+
+    trigger.on('click', this.toggle)
+  },
+
+  toggle(){
+    this.$().toggleClass('open')
+  },
+
+  willDestroyElement(){
+    this.get('triggerEl').off('click', this.toggle)
+  }
+
+});
