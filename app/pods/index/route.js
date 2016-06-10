f47b5632b99d763bac80e78c523f5aad7882442c
Arnaud PEZEL <arnaud@apyx.fr>
2016-06-10 09:16:56 +0200

    redirect index -> project.step1

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -5,6 +5,10 @@ export default Ember.Route.extend({
   renderTemplate: function() {
     this.render({ outlet: "main" });
     this.render("index.header", { outlet: "header" });
+  },
+
+  redirect(model) {
+    this.transitionTo('project.step1');
   }
 
 });
