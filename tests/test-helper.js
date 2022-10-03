7b6b91b329d1bb8296ae8fb726391380ebbd79ad
removedporn <86824510+removedporn@users.noreply.github.com>
2022-10-03 08:25:15 +0800

    Delete tests directory

--- tests/test-helper.js
+++
@@ -1,8 +0,0 @@
-import resolver from './helpers/resolver';
-import registerSelectHelper from './helpers/register-select-helper';
-registerSelectHelper();
-import {
-  setResolver
-} from 'ember-qunit';
-
-setResolver(resolver);
